03496af55578ba5caa21c2eca94a4d2d219f2b3e
Andre Luis Gomes <andrelugomes@gmail.com>
2013-01-23 15:51:10 -0200

    testesno com maven

---
+++ src/test/java/TesteAutomatizado.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TesteAutomatizado {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://www.bing.com");
+		WebElement  campoDeTexto = driver.findElement(By.name("q"));
+		campoDeTexto.sendKeys("Caelum");
+
+		campoDeTexto.submit();
+		
+		driver.close();
+		driver.quit();
+	}
+}
